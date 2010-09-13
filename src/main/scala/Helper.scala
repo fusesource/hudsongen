fa7ed71d180ca0d8f8fb808048faf0ad56bd37ae
Hiram Chirino <hiram@hiramchirino.com>
2010-09-13 11:35:24 -0400

    initial spike

---
+++ src/main/scala/Helper.scala
@@ -0,0 +1,114 @@
+import java.io._
+import org.fusesource.scalate.{TemplateEngine, DefaultRenderContext, Binding}
+import org.fusesource.scalate.support.FileResourceLoader
+import org.fusesource.scalate.util.IOUtil._
+
+trait SCM {
+  def url:String
+  def web_url:Option[String]
+  
+}
+
+class Git(var url:String, var web_url: Option[String] = None, var branches: List[String] = List("master")) extends SCM {
+  def branches(values:String*): this.type = { branches = List(values: _*); this}
+  def web_url(value:Option[String]): this.type = { web_url = value; this}
+}
+class GitHub(var user:String, var project:String) extends Git("git://github.com/"+user+"/"+project+".git", Some("http://github.com/"+user+"/"+project+"")) 
+
+class Subversion(val url:String, val local:String) extends SCM {
+  var web_url:Option[String] = Some(url) 
+  def web_url(value:Option[String]): this.type = { web_url = value; this}
+}
+
+class Project(val name:String, val scm:SCM) {
+  
+  var jdks = List("jdk6")
+  def jdks(values:String*): this.type = { jdks = List(values: _*); this}
+  
+  var labels = List("ubuntu","windows")
+  def labels(values:String*): this.type = { labels = List(values: _*); this}
+  
+  var timeout = 60
+  def timeout(value:Int): this.type = { timeout = value; this}
+
+  def git(proc: (Git)=>Unit): this.type = { proc(scm.asInstanceOf[Git]); this}
+  
+}
+
+object Helper {
+  
+  var jobs_dir:File = _
+  
+  /////////////////////////////////////////////////////////////////////
+  // Config Generators
+  /////////////////////////////////////////////////////////////////////
+
+  def subversion(project:String, url:String) = {
+    new Project(project, new Subversion(url, project))
+  }
+
+  def github(user:String, project:String) = {
+    new Project(project, new GitHub(user, project))
+  }
+
+  def forge_git(project:String) = {
+    new Project(project, new Git("ssh://git@forge.fusesource.com/"+project+".git"))
+  }
+
+  def generate(project:Project) = {
+    checkin(project)
+    platform(project)
+    deploy(project)
+  }
+  
+  def checkin(project:Project) = {
+    job(project.name+"-checkin", render("checkin.jade", Map("project"->project)))
+  }
+  def platform(project:Project) = {
+    job(project.name+"-platform", render("platform.jade", Map("project"->project)))
+  }
+  def deploy(project:Project) = {
+    job(project.name+"-deploy", render("deploy.jade", Map("project"->project)))
+  }
+
+  def with_trailing_slash(value:String) = {
+    if( value.endsWith("/") ) {
+      value
+    } else {
+      value + "/"
+    }
+  }
+
+  /////////////////////////////////////////////////////////////////////
+  // IO Helpers
+  /////////////////////////////////////////////////////////////////////
+  private def job(name:String, config:String) = {
+    write(jobs_dir/name/"config.xml", config)
+  }
+  
+  private def write(file:File, value:Any) = {
+    file.getParentFile.mkdirs
+    writeText(file, value.toString)
+  } 
+  
+  /////////////////////////////////////////////////////////////////////
+  // Scalate Helpers
+  /////////////////////////////////////////////////////////////////////
+  
+  org.fusesource.scalate.scaml.ScamlOptions.autoclose = null
+  private val engine = new TemplateEngine
+  engine.resourceLoader = new FileResourceLoader(Some(new File("./src/main/template")))
+  engine.workingDirectory = new File("./target/scalate")
+  engine.bindings ::= Binding("val project: Project")
+  
+  private def render( template:String, attributes: Map[String,Any]) = {
+    val buffer = new StringWriter()
+    val context = new DefaultRenderContext(engine, new PrintWriter(buffer))
+    for ((key, value) <- attributes) {
+      context.attributes(key) = value
+    }
+    context.include(template, false)
+    buffer.toString
+  }
+
+}
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,95 @@
+import java.io._
+import Helper._
+
+object Main {
+  def main(args:Array[String]):Unit = {
+    if( args.length < 1 ) {
+      println("Expected the jobs directory as an argument")
+      System.exit(1)
+    }
+    jobs_dir = new File(args(0))
+    
+    // Random Support Projects
+    generate(github("scalate", "scalate"))
+    generate(github("chirino", "hawtdb"))
+    generate(github("chirino", "hawtdispatch"))
+    generate(github("chirino", "hawtjni"))
+    generate(github("chirino", "hawtbuf"))
+    generate(github("chirino", "mvnplugins"))
+    generate(github("chirino", "rmiviajms"))
+    generate(github("chirino", "jansi"))
+    generate(github("gnodet", "org.eclipse.mylyn").git(_.branches("origin")))
+    
+    generate(forge_git("jansi"))
+    generate(forge_git("console"))
+    generate(forge_git("mop"))
+
+    // ActiveMQ Branches
+    generate(activemq("trunk-fusesource"))
+    generate(activemq("5.4-fusesource"))
+    generate(subversion("activemq-5.3.1-fuse", "http://fusesource.com/forge/svn/fusemq/branches/activemq-5.3.1-fuse"))
+
+    // Camel Branches
+    generate(subversion("camel-trunk-fuse", "http://fusesource.com/forge/svn/fuseeip/trunk"))
+    generate(subversion("camel-2.2.0-fuse", "http://fusesource.com/forge/svn/fuseeip/branches/camel-2.2.0-fuse"))
+    generate(subversion("camel-1.x-fuse", "http://fusesource.com/forge/svn/fuseeip/branches/camel-1.x-fuse"))
+
+    // CXF Branches
+    generate(subversion("cxf-trunk-fuse", "http://fusesource.com/forge/svn/fusesf/trunk"))
+    generate(subversion("cxf-2.2.x-fuse", "http://fusesource.com/forge/svn/fusesf/branches/cxf-2.2.x-fuse"))
+
+    // ServiceMix Branches
+    generate(smx4_nmr("trunk"))
+    generate(smx4_nmr("nmr-1.2.0-fuse"))
+    generate(smx4_nmr("nmr-1.3.0-fuse"))
+
+    generate(smx4_features("trunk"))
+    generate(smx4_features("features-4.2.0-fuse"))
+    generate(smx4_features("features-4.3.0-fuse"))
+    
+    generate(smx_maven_plugins("trunk"))
+    generate(smx_maven_plugins("maven-plugins-4.3.0-fusesource"))
+    
+    generate(smx_utils("trunk"))
+
+    generate(smx_components("trunk"))
+    generate(smx_components("components-2010.02.0-fuse"))
+    generate(smx_components("components-2010.01.0-fuse"))
+    generate(smx_components("components-2009.01.x"))
+
+    // The specks don't have tests do don't need a nightly.
+    var specs = subversion("smx4-specs-trunk-fuse", "http://fusesource.com/forge/svn/fuseesb/smx4/specs/trunk")
+    checkin(specs)
+    deploy(specs)
+
+  }
+  
+  def activemq(branch:String) = new Project("activemq-"+branch, new Git("ssh://git@forge.fusesource.com/activemq.git", None, List(branch)))
+
+  val smx_base = "http://fusesource.com/forge/svn/fuseesb"
+  def smx4_nmr(branch:String) =  branch match {
+    case "trunk" => subversion("smx4-nmr-trunk-fuse", smx_base+"/smx4/nmr/trunk")
+    case branch => subversion("smx4-"+branch, smx_base+"/smx4/nmr/branches/"+branch)
+  }
+
+  def smx4_features(branch:String) =  branch match {
+    case "trunk" => subversion("smx4-features-trunk-fuse", smx_base+"/smx4/features/trunk")
+    case branch => subversion("smx4-"+branch, smx_base+"/smx4/features/branches/"+branch)
+  }
+  
+  def smx_maven_plugins(branch:String) =  branch match {
+    case "trunk" => subversion("smx-maven-plugins-trunk-fuse", smx_base+"/maven-plugins/trunk")
+    case branch => subversion("smx-"+branch, smx_base+"/maven-plugins/branches/"+branch)
+  }
+  
+  def smx_utils(branch:String) =  branch match {
+    case "trunk" => subversion("smx-utils-trunk-fuse", smx_base+"/utils/trunk")
+    case branch => subversion("smx-"+branch, smx_base+"/utils/branches/"+branch)
+  }
+
+  def smx_components(branch:String) =  branch match {
+    case "trunk" => subversion("smx-components-trunk-fuse", smx_base+"/components/trunk")
+    case branch => subversion("smx-"+branch, smx_base+"/components/branches/"+branch)
+  }
+
+}
