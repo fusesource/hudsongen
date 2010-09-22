08d9367d31408b6afc277008681325297f788721
James Strachan <james.strachan@gmail.com>
2010-09-22 10:37:06 +0100

    refactored the code a little so the DSL is much simpler & more powerful (e.g. its easy to tinker with a project) as well as making it easier to add/remove builds, configure the profiles used on a specific build or customize a project.
    e.g.
    
    github("foo", "foo").using{p =>
      p.checkin.mavenOptions = MavenOptions(List("someProfile"))
      p.deploy.mavenOptions = MavenOptions(List("another"))
    }
    
    or
    
    github("blah", "whatnot").removeBuild(_.platform)
    
    also much of the 'main' glue code is now inherited, so its easy to move the Helper into a library one day

--- src/main/scala/Helper.scala
+++ src/main/scala/Helper.scala
@@ -1,3 +1,4 @@
+import collection.mutable.ListBuffer
 import java.io._
 import org.fusesource.scalate.{TemplateEngine, DefaultRenderContext, Binding}
 import org.fusesource.scalate.support.FileResourceLoader
@@ -13,14 +14,28 @@ class Git(var url:String, var web_url: Option[String] = None, var branches: List
   def branches(values:String*): this.type = { branches = List(values: _*); this}
   def web_url(value:Option[String]): this.type = { web_url = value; this}
 }
-class GitHub(var user:String, var project:String) extends Git("git://github.com/"+user+"/"+project+".git", Some("http://github.com/"+user+"/"+project+"")) 
+
+class GitHub(var user:String, var project:String) extends Git("git://github.com/"+user+"/"+project+".git", Some("http://github.com/"+user+"/"+project+""))
 
 class Subversion(val url:String, val local:String) extends SCM {
   var web_url:Option[String] = Some(url) 
   def web_url(value:Option[String]): this.type = { web_url = value; this}
 }
 
-class Project(val name:String, val scm:SCM) {
+case class MavenOptions(profiles: List[String] = Nil) {
+
+  /**
+   * Returns the command line argument for profiles
+   */
+  def goalsArguments = profiles.map("-P" + _).mkString(" ")
+}
+
+case class Build(name: String) {
+  var mavenOptions = MavenOptions()
+  def mavenOptions(value: MavenOptions): this.type = { mavenOptions = value; this }
+}
+
+case class Project(val name:String, val scm:SCM) {
   
   var jdks = List("jdk6")
   def jdks(values:String*): this.type = { jdks = List(values: _*); this}
@@ -31,52 +46,118 @@ class Project(val name:String, val scm:SCM) {
   var timeout = 60
   def timeout(value:Int): this.type = { timeout = value; this}
 
-  def git(proc: (Git)=>Unit): this.type = { proc(scm.asInstanceOf[Git]); this}
-  
+  def git(proc: (Git)=>Unit): this.type = { proc(scm.asInstanceOf[Git]); this }
+
+
+  // Helper methods
+
+  /**
+   * Lets the project be configured using a block which avoids global variables
+   * needing to be defined for each project, instead its a local scoped parameter
+   */
+  def using(f: Project => Unit): this.type = {
+    f(this)
+    this
+  }
+
+  // builds
+  val checkin = Build("checkin")
+  val platform = Build("platform")
+  val deploy = Build("deploy")
+
+  var builds: List[Build] = List(checkin, platform, deploy)
+
+  def removeBuild(b: Build): this.type =  {
+    builds = builds filterNot(_ == b)
+    this
+  }
+
+  def removeBuild(f: Project => Build): this.type =
+    removeBuild(f(this))
+
+  def removeBuilds(list: List[Build]): this.type =  {
+    builds = builds filterNot(list contains)
+    this
+  }
+
+  def removeBuilds(f: Project => List[Build]): this.type =
+    removeBuilds(f(this))
 }
 
 object Helper {
-  
-  var jobs_dir:File = _
-  
-  /////////////////////////////////////////////////////////////////////
-  // Config Generators
-  /////////////////////////////////////////////////////////////////////
-
-  def subversion(project:String, url:String) = {
-    new Project(project, new Subversion(url, project))
-  }
-
-  def github(user:String, project:String) = {
-    new Project(project, new GitHub(user, project))
-  }
-
-  def forge_git(project:String) = {
-    new Project(project, new Git("ssh://git@forge.fusesource.com/"+project+".git"))
-  }
-
-  def generate(project:Project) = {
-    checkin(project)
-    platform(project)
-    deploy(project)
-  }
-  
-  def checkin(project:Project) = {
-    job(project.name+"-checkin", render("checkin.jade", Map("project"->project)))
-  }
-  def platform(project:Project) = {
-    job(project.name+"-platform", render("platform.jade", Map("project"->project)))
-  }
-  def deploy(project:Project) = {
-    job(project.name+"-deploy", render("deploy.jade", Map("project"->project)))
-  }
-
   def with_trailing_slash(value:String) = {
     if( value.endsWith("/") ) {
       value
     } else {
       value + "/"
     }
+  }
+}
+
+abstract class Helper {
+  
+  var jobs_dir:File = _
+
+  val projects = ListBuffer[Project]()
+
+  def add(project: Project): Project = {
+    projects += project
+    project
+  }
+
+  /**
+   * Configures the model
+   */
+  protected def configure: Unit
+
+
+  /**
+   * Runs the command line shell
+   */
+  def main(args: Array[String]): Unit = {
+    // TODO should we use Karaf annotations for this stuff?
+    if (args.length < 1) {
+      println("Expected the jobs directory as an argument")
+      System.exit(1)
+    }
+    jobs_dir = new File(args(0))
+    configure
+
+    generate
+  }
+
+
+  /////////////////////////////////////////////////////////////////////
+  // Config Generators
+  /////////////////////////////////////////////////////////////////////
+
+  def subversion(project:String, url:String) = {
+    add(Project(project, new Subversion(url, project)))
+  }
+
+  def github(user:String, project:String) = {
+    add(Project(project, new GitHub(user, project)))
+  }
+
+  def forge_git(project:String) = {
+    add(new Project(project, new Git("ssh://git@forge.fusesource.com/"+project+".git")))
+  }
+
+  def generate(): Unit = {
+    for (project <- projects) {
+      generate(project)
+    }
+  }
+
+  def generate(project:Project): Unit = {
+    for (b <- project.builds) {
+      build(project, b)
+    }
+  }
+  
+  def build(project:Project, build: Build): Unit = {
+    val name = build.name
+    job(project.name + "-" + name, render(name + ".jade", Map("project" -> project, "build" -> build)))
   }
 
   /////////////////////////////////////////////////////////////////////
@@ -99,8 +180,8 @@ object Helper {
   private val engine = new TemplateEngine
   engine.resourceLoader = new FileResourceLoader(Some(new File("./src/main/template")))
   engine.workingDirectory = new File("./target/scalate")
-  engine.bindings ::= Binding("val project: Project")
-  
+  engine.bindings ++= List(Binding("val project: Project"), Binding("var build: Build"))
+
   private def render( template:String, attributes: Map[String,Any]) = {
     val buffer = new StringWriter()
     val context = new DefaultRenderContext(null, engine, new PrintWriter(buffer))
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,67 +1,58 @@
 import java.io._
-import Helper._
 
-object Main {
-  def main(args:Array[String]):Unit = {
-    if( args.length < 1 ) {
-      println("Expected the jobs directory as an argument")
-      System.exit(1)
-    }
-    jobs_dir = new File(args(0))
-    
+object Main extends Helper {
+
+  protected def configure = {
     // Random Support Projects
-    generate(github("scalate", "scalate"))
-    generate(github("chirino", "hawtdb"))
-    generate(github("chirino", "hawtdispatch"))
-    generate(github("chirino", "hawtjni"))
-    generate(github("chirino", "hawtbuf"))
-    generate(github("chirino", "mvnplugins"))
-    generate(github("chirino", "rmiviajms"))
-    generate(github("chirino", "jansi"))
-    generate(github("gnodet", "wikitext").git(_.branches("origin")))
+    github("scalate", "scalate").checkin.mavenOptions(MavenOptions(List("download")))
+    github("chirino", "hawtdb")
+    github("chirino", "hawtdispatch")
+    github("chirino", "hawtjni")
+    github("chirino", "hawtbuf")
+    github("chirino", "mvnplugins")
+    github("chirino", "rmiviajms")
+    github("chirino", "jansi")
+    github("gnodet", "wikitext").git(_.branches("origin"))
     
-    generate(forge_git("jansi"))
-    generate(forge_git("console"))
-    generate(forge_git("mop"))
+    forge_git("jansi")
+    forge_git("console")
+    forge_git("mop")
 
     // ActiveMQ Branches
-    generate(activemq("trunk-fusesource").timeout(2*60))
-    generate(activemq("5.4-fusesource").timeout(2*60))
-    generate(subversion("activemq-5.3.1-fuse", "http://fusesource.com/forge/svn/fusemq/branches/activemq-5.3.1-fuse").timeout(2*60))
+    activemq("trunk-fusesource").timeout(2*60)
+    activemq("5.4-fusesource").timeout(2*60)
+    subversion("activemq-5.3.1-fuse", "http://fusesource.com/forge/svn/fusemq/branches/activemq-5.3.1-fuse").timeout(2*60)
 
     // Camel Branches
-    generate(subversion("camel-trunk-fuse", "http://fusesource.com/forge/svn/fuseeip/trunk").timeout(3*60))
-    generate(subversion("camel-2.2.0-fuse", "http://fusesource.com/forge/svn/fuseeip/branches/camel-2.2.0-fuse").timeout(3*60))
-    generate(subversion("camel-1.x-fuse", "http://fusesource.com/forge/svn/fuseeip/branches/camel-1.x-fuse").timeout(3*60))
+    subversion("camel-trunk-fuse", "http://fusesource.com/forge/svn/fuseeip/trunk").timeout(3*60)
+    subversion("camel-2.2.0-fuse", "http://fusesource.com/forge/svn/fuseeip/branches/camel-2.2.0-fuse").timeout(3*60)
+    subversion("camel-1.x-fuse", "http://fusesource.com/forge/svn/fuseeip/branches/camel-1.x-fuse").timeout(3*60)
 
     // CXF Branches
-    generate(subversion("cxf-trunk-fuse", "http://fusesource.com/forge/svn/fusesf/trunk"))
-    generate(subversion("cxf-2.2.x-fuse", "http://fusesource.com/forge/svn/fusesf/branches/cxf-2.2.x-fuse"))
+    subversion("cxf-trunk-fuse", "http://fusesource.com/forge/svn/fusesf/trunk")
+    subversion("cxf-2.2.x-fuse", "http://fusesource.com/forge/svn/fusesf/branches/cxf-2.2.x-fuse")
 
     // ServiceMix Branches
-    generate(smx4_nmr("trunk"))
-    generate(smx4_nmr("nmr-1.2.0-fuse"))
-    generate(smx4_nmr("nmr-1.3.0-fuse"))
+    smx4_nmr("trunk")
+    smx4_nmr("nmr-1.2.0-fuse")
+    smx4_nmr("nmr-1.3.0-fuse")
 
-    generate(smx4_features("trunk"))
-    generate(smx4_features("features-4.2.0-fuse"))
-    generate(smx4_features("features-4.3.0-fuse"))
+    smx4_features("trunk")
+    smx4_features("features-4.2.0-fuse")
+    smx4_features("features-4.3.0-fuse")
     
-    generate(smx_maven_plugins("trunk"))
-    generate(smx_maven_plugins("maven-plugins-4.3.0-fusesource"))
+    smx_maven_plugins("trunk")
+    smx_maven_plugins("maven-plugins-4.3.0-fusesource")
     
-    generate(smx_utils("trunk"))
+    smx_utils("trunk")
 
-    generate(smx_components("trunk").timeout(2*60))
-    generate(smx_components("components-2010.02.0-fuse").timeout(2*60))
-    generate(smx_components("components-2010.01.0-fuse").timeout(2*60))
-    generate(smx_components("components-2009.01.x").timeout(2*60))
+    smx_components("trunk").timeout(2*60)
+    smx_components("components-2010.02.0-fuse").timeout(2*60)
+    smx_components("components-2010.01.0-fuse").timeout(2*60)
+    smx_components("components-2009.01.x").timeout(2*60)
 
-    // The specks don't have tests do don't need a nightly.
-    var specs = subversion("smx4-specs-trunk-fuse", "http://fusesource.com/forge/svn/fuseesb/smx4/specs/trunk")
-    checkin(specs)
-    deploy(specs)
-
+    // The specks don't have tests so don't need a nightly.
+    subversion("smx4-specs-trunk-fuse", "http://fusesource.com/forge/svn/fuseesb/smx4/specs/trunk").removeBuild(_.platform)
   }
   
   def activemq(branch:String) = new Project("activemq-"+branch, new Git("ssh://git@forge.fusesource.com/activemq.git", None, List(branch)))
