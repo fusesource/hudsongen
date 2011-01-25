b995654d4cf49839dcef75a86a2542804d41f6b3
Gert Vanthienen <gert.vanthienen@gmail.com>
2011-01-25 21:24:05 +0100

    Renamed branch to utils-1.4.x-fuse

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -78,7 +78,7 @@ object Main extends Helper {
     
     smx_utils("trunk")
     smx_utils("utils-1.3.0-fuse")
-    esb_utils("utils-1.3.1-fuse")
+    esb_utils("utils-1.4.x-fuse")
 
     smx_components("trunk").timeout(2*60)
     smx_components("components-2010.02.0-fuse").timeout(2*60)
