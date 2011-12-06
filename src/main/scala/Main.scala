0c458bfda22d0ae9f10c699fe04c242817053e7a
Willem Jiang <willem.jiang@gmail.com>
2011-12-06 17:35:43 +0800

    Added 2.5.x-fuse branch

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -89,6 +89,7 @@ object Main extends Helper {
     cxf("2.2.6-fuse").disable
     cxf("2.3.x-fuse")
     cxf("2.4.x-fuse") using ( perfectus("cxf", _) ) 
+    cxf("2.5.x-fuse") using ( perfectus("cxf", _) )
     cxf("2.4.2-fuse-00-xx") using ( perfectus("cxf", _) ) 
     
     //esbsystemtests
