92a97819e9b7bf5256cc1da80a8b26b8ed71d09a
Kevin Earls <kevin@kevinearls.com>
2013-02-20 08:26:03 -0500

    Increased timeout for activemq tests because of slow aix and solaris boxes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -659,7 +659,7 @@ object Main extends Helper {
 
   def activemq(branch:String) =  {
     val project = new Project("activemq-"+branch, new Git("ssh://git@forge.fusesource.com/activemq.git", None, List(branch)))
-    project.timeout(10*60)
+    project.timeout(15*60)
     project.deploy.timeout(60)
     project.mavenName("maven-3.0.2")
     add(project)
